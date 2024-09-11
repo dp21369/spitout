efcbe25f90fc8e7d9c8b30d4ef2d4e60805c5fb1
Chakramani Joshi <chakramanijoshi@gmail.com>
2024-09-11 09:14:16 +0545

    minor change in home page review system

--- functions.php
+++ functions.php
@@ -2492,20 +2492,27 @@ function so_banner_content()
 
     <div class="banner-rewiews">
         <div class="reviewers-img">
-            <?php if (!empty($user_ids_with_5_star))
-                foreach ($user_ids_with_5_star as $user_id) {
-                    $attachment_id = (int) get_user_meta($user_id, 'so_profile_img', true);
+            <?php //if (!empty($user_ids_with_5_star))
+                //foreach ($user_ids_with_5_star as $user_id) {
+                    //$attachment_id = (int) get_user_meta($user_id, 'so_profile_img', true);
                     // $seller_img_url = resize_and_compress_image($attachment_id, 150, 150, 70); 
-                    $seller_img_url = wp_get_attachment_image_src($attachment_id, 'thumbnail');
-                    if (!$seller_img_url) {
+                    // $seller_img_url = wp_get_attachment_image_src($attachment_id, 'thumbnail');
+                    // if (!$seller_img_url) {
                         $seller_img_url = get_template_directory_uri() . '/assets/img/user.png';
-                    } else {
-                        $seller_img_url = $seller_img_url[0];
-                    } ?>
+                    // } else {
+                    //     $seller_img_url = $seller_img_url[0];
+                    // } 
+                    ?>
                 <figure>
                     <img src="<?php echo esc_url($seller_img_url); ?>" alt="reviewer-image">
                 </figure>
-            <?php } ?>
+                <figure>
+                    <img src="<?php echo esc_url($seller_img_url); ?>" alt="reviewer-image">
+                </figure>
+                <figure>
+                    <img src="<?php echo esc_url($seller_img_url); ?>" alt="reviewer-image">
+                </figure>
+            <?php //} ?>
         </div>
         <div class="review-details">
             <div class="review-icons">
@@ -2518,7 +2525,7 @@ function so_banner_content()
                 <span>5.0</span>
             </div>
             <div class="review-text">
-                <p>From <?php echo $unique_user_count; ?>+ Reviews</p>
+                <p>From <?php //echo $unique_user_count; ?> 150+ Reviews</p>
             </div>
         </div>
     </div>
@@ -2548,8 +2555,8 @@ function so_category_list()
                 )
             );
 
-            foreach ($get_spit_category as $key => $get_spit_cat) { 
-                $post_slug = get_post_field('post_name', $get_spit_cat->ID);?>
+            foreach ($get_spit_category as $key => $get_spit_cat) {
+                $post_slug = get_post_field('post_name', $get_spit_cat->ID); ?>
                 <li>
                     <a href="<?php echo '/seller/?cat=' . esc_html($post_slug); ?>">
                         <h5><?php echo esc_html($get_spit_cat->post_title); ?></h5>
@@ -3010,25 +3017,26 @@ function send_completed_order_email_to_admin($recipient, $order)
 //===========change woocomerce email template recipient ends======================
 
 
-function redirect_spit_category_to_seller() {
+function redirect_spit_category_to_seller()
+{
     // Get the current URL path
-    $current_url = home_url( add_query_arg( null, null ) );
+    $current_url = home_url(add_query_arg(null, null));
 
     // Check if the URL contains '/spit-category/'
-    if ( strpos( $current_url, '/spit-category/' ) !== false ) {
+    if (strpos($current_url, '/spit-category/') !== false) {
         // Extract the category slug from the URL
-        $parts = explode( '/spit-category/', $current_url );
+        $parts = explode('/spit-category/', $current_url);
 
-        if ( isset( $parts[1] ) && !empty( $parts[1] ) ) {
-            $category_slug = trim( $parts[1], '/' );
+        if (isset($parts[1]) && !empty($parts[1])) {
+            $category_slug = trim($parts[1], '/');
 
             // Build the new URL
-            $new_url = home_url( '/seller/?cat=' . $category_slug );
+            $new_url = home_url('/seller/?cat=' . $category_slug);
 
             // Perform the redirect
-            wp_redirect( $new_url );
+            wp_redirect($new_url);
             exit();
         }
     }
 }
-add_action( 'template_redirect', 'redirect_spit_category_to_seller' );
+add_action('template_redirect', 'redirect_spit_category_to_seller');
